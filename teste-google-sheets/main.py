eda187de4af843b05fbc701d3ad6688f11ef494e
Netto011 <arlindogoncalvesdearaujoneto@gmail.com>
2024-08-25 17:54:55 -0300

    formulário ravlt

--- teste-google-sheets/main.py
+++ teste-google-sheets/main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # Renderiza o template HTML RAVLT.html localizado na pasta 'templates'
     return render_template('RAVLT.html')
 
 if __name__ == '__main__':
+    # Inicia o servidor Flask no modo de depuração para facilitar o desenvolvimento
     app.run(debug=True)
